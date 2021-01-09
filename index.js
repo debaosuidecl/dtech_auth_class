33cd80caa056c54ce728f8a19b69488e7a52e8f0
Osuide Osedebamen <debaosuidecl@gmail.com>
2021-01-09 14:16:43 +0100

    D

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const auth = require("./routes/auth")
+const MONGO_URI = "mongodb+srv://deba:test123@cluster0.mtqmn.mongodb.net/authnode?retryWrites=true&w=majority"
+app.use(express.json());
+
+
+app.use("/api/auth", auth); // redirect to auth route
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+}).then((res)=>{
+        console.log("Yay we have connected to mongodb")
+        app.listen(PORT, ()=>{
+            console.log(`Listening on port ${PORT}`)
+        })
+}).catch((err)=>{
+    console.log(err)
+})
